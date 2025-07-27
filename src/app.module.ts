4d3ef8a29319e1a6c784fabbe6f7a7dfa87381f8
ihorhnennyi <ihorhnennyi@gmail.com>
2025-07-27 14:56:50 +0300

    feat(app): connected AuthModule and UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
+
 import configuration from './config/configuration'
 import { validationSchema } from './config/validation'
+
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './users/user.module'
 
 @Module({
 	imports: [
@@ -17,6 +21,8 @@ import { validationSchema } from './config/validation'
 				uri: config.get<string>('mongodbUri'),
 			}),
 		}),
+		AuthModule,
+		UserModule,
 	],
 })
 export class AppModule {}
